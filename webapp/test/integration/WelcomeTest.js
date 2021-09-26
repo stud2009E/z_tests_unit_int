ee011a4958cfc9e5402e9bce4bd7377617e450ce
unknown <stud2009@yandex.ru>
2021-09-26 22:30:24 +0300

    add sort integration tests

--- webapp/test/integration/WelcomeTest.js
+++ webapp/test/integration/WelcomeTest.js
@@ -6,30 +6,61 @@ sap.ui.define([
 
 	QUnit.module("Welcome test");
 
-	opaTest("Should start the app and see table with 10 rows", function (Given, When, Then) {
-		// Arrangements
-		Given.iStartMyApp();
-		// Assertions
-		Then.onMain.iShouldSee10EntriesInSmartTable();
-        // Cleanup
-		Then.iTeardownMyApp();
-	});
+	// opaTest("Should start the app and see table with 10 rows", function (Given, When, Then) {
+	// 	// Arrangements
+	// 	Given.iStartMyApp();
+	// 	// Assertions
+	// 	Then.onMain.iShouldSee10EntriesInSmartTable();
+    //     // Cleanup
+	// 	Then.iTeardownMyApp();
+	// });
 
-	QUnit.module("Column test");
-	opaTest("Check columns ", function (Given, When, Then) {
+	// QUnit.module("Column test");
+	// opaTest("Check columns ", function (Given, When, Then) {
+	// 	// Arrangements
+	// 	Given.iStartMyApp();
+		
+	// 	// Assertions
+	// 	Then.onMain.iShouldSee10EntriesInSmartTable()
+	// 		.iShouldSee8ColumnsInSmartTable();
+	// 	When.onMain.pressTableSettings();
+	// 	Then.onMain.iShouldSeeTableSettingsDialog();
+		
+	// 	When.onMain.selectAllColumns()
+	// 		.applyTableSettings();
+
+	// 	Then.onMain.iShouldSee9ColumnsInSmartTable();
+
+    //     // Cleanup
+	// 	Then.iTeardownMyApp();
+	// });
+
+
+	QUnit.module("Sort test");
+	opaTest("Sort by Decimal ", function (Given, When, Then) {
 		// Arrangements
 		Given.iStartMyApp();
 		
-		// Assertions
-		Then.onMain.iShouldSee10EntriesInSmartTable()
-			.iShouldSee8ColumnsInSmartTable();
 		When.onMain.pressTableSettings();
 		Then.onMain.iShouldSeeTableSettingsDialog();
 		
-		When.onMain.selectAllColumns()
+		When.onMain.selectSortTabInSettingDialog()
+			.chooseSortFieldInSortDialog("Decimal")
+			.chooseSortType(true)
 			.applyTableSettings();
 
-		Then.onMain.iShouldSee9ColumnsInSmartTable();
+		Then.onMain.iShouldSortedTableBy("Decimal", true);
+		
+		When.onMain.pressTableSettings();
+		Then.onMain.iShouldSeeTableSettingsDialog();
+
+		When.onMain.selectSortTabInSettingDialog()
+			.chooseSortFieldInSortDialog("Decimal")
+			.chooseSortType(false)
+			.applyTableSettings();
+
+		Then.onMain.iShouldSortedTableBy("Decimal", false);
+		
 
         // Cleanup
 		Then.iTeardownMyApp();
--- webapp/test/integration/pages/Welcome.js
+++ webapp/test/integration/pages/Welcome.js
@@ -2,12 +2,18 @@ sap.ui.define([
 	'sap/ui/test/Opa5',
 	'sap/ui/test/matchers/AggregationLengthEquals',
 	"sap/ui/test/matchers/PropertyStrictEquals",
-	"sap/ui/test/actions/Press"
+	"sap/ui/test/actions/Press",
+	"sap/ui/test/actions/EnterText",
+	"sap/ui/test/matchers/Ancestor",
+	"sap/ui/test/matchers/Properties",
 ], function (
 	Opa5,
 	AggregationLengthEquals,
 	PropertyStrictEquals,
-	Press
+	Press,
+	EnterText,
+	Ancestor,
+	Properties
 ) {
 	"use strict";
 
@@ -31,6 +37,7 @@ sap.ui.define([
 					});
 				},
 
+				//TODO replace by one action with param 
 				iShouldSee8ColumnsInSmartTable: function () {
 					return this.waitFor({
 						controlType: "sap.ui.table.Table",
@@ -74,7 +81,42 @@ sap.ui.define([
 						},
 						errorMessage : "Can not find setting dialog"
 					})
+				},
+
+				iShouldSortedTableBy: function(sField, bAsc){
+					var sText = bAsc ? "По восходящей" : "По нисходящей";
+
+					return this.waitFor({
+						controlType: "sap.ui.table.Table",
+						matchers: new AggregationLengthEquals({
+							length : 10,
+							name: "rows"
+						}),
+						success : function (aTables) {
+							var oTable = aTables[0];
+							var oModel = oTable.getModel();
+							var aDataSet = [];
+							$.each(oModel.oData, function(sKey, oObject){
+								if(sKey.startsWith("TestDataSet")){
+									aDataSet.push(oObject[sField]);
+								}
+							});
+
+							var fnSort = bAsc ? (a, b) => a - b : (a, b) => - (a - b);
+							aDataSet.sort(fnSort);
+
+							var oBinding = oTable.getBinding("rows");
+							var aSortData = oBinding.getContexts()
+								.map(function(oCtx){
+									return oCtx.getProperty("Decimal");
+								});
+							
+							Opa5.assert.deepEqual(aDataSet, aSortData, "Data in smart Table are corectly sorted " + sText);
+						},
+						errorMessage : "The smart table has incorrect sort order"
+					});
 				}
+
 			},
 
 			actions: {
@@ -83,6 +125,51 @@ sap.ui.define([
 						id : "smartTable-btnPersonalisation",
 						actions : new Press(),
 						errorMessage : "did not find the Button"
+					});
+				},
+
+				selectSortTabInSettingDialog: function(){
+					return this.waitFor({
+						controlType : "sap.m.Button",
+						searchOpenDialogs: true,
+						matchers: new PropertyStrictEquals({
+                            name : "text",
+							value: "Сортировка"
+                        }),
+						actions : new Press(),
+						errorMessage : "did not find the Sort button Button"
+					});
+				},
+
+				chooseSortFieldInSortDialog: function(sText){
+					return this.waitFor({
+						controlType : "sap.m.ComboBox",
+						searchOpenDialogs: true,
+						actions: new EnterText({ text: sText }),
+						errorMessage : "did not find the sort field combobox"
+					});
+				},
+
+				chooseSortType: function(bAsc){
+					var sText = bAsc ? "По восходящей" : "По нисходящей";
+
+					return this.waitFor({
+						controlType : "sap.m.Select",
+						actions: new Press(),
+						searchOpenDialogs: true,
+						success: function(oSelect) {
+							this.waitFor({
+								controlType: "sap.ui.core.Item",
+								searchOpenDialogs: true,
+								matchers: [
+									new Ancestor(oSelect[0]),
+									new Properties({ text: sText})
+								],
+								actions: new Press(),
+								errorMessage: "Cannot select " + sText + " from sort select"
+							});
+						},
+						errorMessage: "Could not find sort select"
 					});
 				},
 
